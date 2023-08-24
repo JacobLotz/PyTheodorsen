501ead01ab47e5d253456b963dfb8d824e4d3a47
Jacob Lotz <jacob.lotz@gmail.com>
2023-08-24 20:57:43 +0200

    clean up

--- theodorsen.py
+++ theodorsen.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy.special as scsp
 
@@ -6,7 +5,6 @@ import scipy.special as scsp
 """
 Theodorsen function and resulting phase and ampl
 """
-
 def theo_fun(k):
     r"""Returns the value of Theodorsen's function at a reduced frequency :math:`k`.
 
@@ -60,7 +58,6 @@ def HeaveAcceleration(omega, A, t):
 """
 Motion functions of the foil: easier to use
 """
-
 def HeaveMotionSimple(T, A, n):
     t = np.linspace(0,T,n)
     omega = 2*np.pi/T
@@ -77,7 +74,6 @@ def HeaveAccelerationSimple(T, A, n):
     return HeaveAcceleration(omega, A, t)
 
 
-
 """
 Theodoorsen lift functions --> Easier: use the simple functions
 """
@@ -91,6 +87,7 @@ def CLCirc(omega, A, t, k):
 
 def CLTotal(omega, A, t, t_cir, k):
 	return CLAddedMass(omega, A, t_cir) + CLCirc(omega, A, t, k)
+
 
 """
 Simple functions that requires an easier input for for instance external scripts
@@ -107,8 +104,6 @@ def CLCircSimple(T, A, n):
     t_cir = t + TheoPhase(k, T) # phase --> frame of reference thing
     omega = 2*np.pi/T	
     return CLCirc(omega, A, t_cir, k)
-
-
 
 def CLTotalSimple(T, A, n):
     """
@@ -135,47 +130,3 @@ def AlphaSimple(T, A, n):
 
 def CLQTSimple(T, A, n):
     return 2*np.pi*AlphaSimple(T, A, n)
-
-"""
-Example
-"""
-
-"""
-# Input
-k = 2.0
-extrusion = np.pi/k
-A = 0.1;               # Heave amplitude
-n = 50				   # Number of time steps
-
-# extrusion = 10*np.pi   # Period T
-
-# Calc var
-t = np.linspace(0,extrusion, n)
-k = np.pi/extrusion
-
-# Plot
-fig, axs = plt.subplots(1)
-axs.set_ylabel(r'$C_L$ / heave motion' )
-axs.set_xlabel('t')
-
-
-plt.plot(t, CLAddedMassSimple(extrusion, A, n), color='red', linestyle=':', linewidth = 1.0, marker='', markersize='3', label = r"$C_l$ AM")
-plt.plot(t, CLCircSimple(extrusion, A, n), color='green', linestyle=':', linewidth = 1.0, marker='', markersize='3',  label = r"$C_l$ circ")
-plt.plot(t, CLTotalSimple(extrusion, A, n), label = r"$C_l$ total")
-plt.plot(t, HeaveMotionSimple(extrusion, A, n),  linewidth = 1, color = 'black',  label = r"$y$")
-plt.plot(t, HeaveVelocitySimple(extrusion, A, n),  linewidth = 1, color = 'black',  linestyle ="--", label = r"$\dot{y}$")
-plt.plot(t, HeaveAccelerationSimple(extrusion, A, n),  linewidth = 1, color = 'black',  linestyle =":", label = r"$\ddot{y}$")
-
-
-# Shrink current axis by 20%
-box = axs.get_position()
-axs.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-
-# Put a legend to the right of the current axis
-axs.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-plt.title("k = " + str(k) + ' -- theo phase = ' + "{:.3f}".format(TheoPhase(k, extrusion)) + ' [s] -- theo fact = ' + "{:.3f}".format(TheoAmpl(k)), fontsize=10)
-plt.savefig('potential-theo/k'+str(k)+'.png', bbox_inches='tight')
-plt.show();
-
-"""
